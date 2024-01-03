cb5f1a6493a8f996d4bc3760e898b4427dd9a165
thebizzle81 <146577297+thebizzle81@users.noreply.github.com>
2024-01-02 23:34:13 -0500

    exp 01

---
+++ scratch1.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const jesseBrenner = "Happy"
+console.log(jesseBrenner)
+
+let a = 12
+let b = 20
+
+// function letterSoup (
+// console.log("A is " (a) "and B is" (b))
+// )
+//
+const alephBet = (a + b)
+console.log(a+b)
+
+if (alephBet === 32) {
+    console.log("We are " + jesseBrenner + " and equal to " + alephBet )
+}
+
+else (alephBet !== 32); {
+    console.log("We are not " + jesseBrenner + " and not equal to", alephBet )
+}
+
+b = 25
+
+
+// if (students[0] === "Bob") {
+//     console.log(students[0] + " is in class!");
+// }
