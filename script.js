3510b8409846c4f1be358a73cc98ce97e7ed49f7
thebizzle81 <146577297+thebizzle81@users.noreply.github.com>
2024-01-17 06:22:36 -0500

    6th try

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // 2024 01 17 0423 //
 
+// There might be a more elegant way of including all possible numbers, symbols, and letters (upperCase and lowerCase) as possible characters when the password is generated, but writing them all out by hand seemed to give me the least amount of problems
 const keys = {
   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
   number: "0123456789",
   symbol: "!@#$%^&*()_+~\\`|}{[]:;?><,./-="
 };
+
+// Here is where the Math. features that generate random characters are Returned for storage
+// The getKey() method of the PushSubscription interface returns an ArrayBuffer representing a client public key, which can then be sent to a server and used in encrypting push message data.
+// The return statement ends function execution and specifies a value to be returned to the function caller.
 const getKey = [
   function upperCase() {
     return keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
@@ -20,7 +25,9 @@ const getKey = [
     return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
   }
 ];
-// maybe use document.querySelector and generateBtn.addEventListener from ORIGINAL
+
+// The Challenge suggested using document.querySelector and generateBtn.addEventListener but document.getElementById seemed to work the best
+// getElementById
 function createPassword() {
   const upper = document.getElementById("upperCase").checked;
   const lower = document.getElementById("lowerCase").checked;
@@ -30,6 +37,8 @@ function createPassword() {
     alert("Please check at least one box!");  
     return;
   }
+
+  // Building the passwordBox to collect all the Returned getElementById's
   const passwordBox = document.getElementById("passwordBox");
   const length = document.getElementById("length");
   // check length value is not too big or too small 
@@ -42,6 +51,11 @@ function createPassword() {
     }
   }
   passwordBox.innerHTML = password;
+
+  // Took a lot of experimentation to make copyPassword do what it is supposed to do, so I pulled some of this code from other examples I found online and see if I could integrate them into my project. Some of this code is advanced from where we are in the course, but I eventually got it to work, so while I still need to study some of these methods etc. some more to fully understand them, I decided to use them for my own purposes for now.
+  // createElement
+  // The <textarea> HTML element represents a multi-line plain-text editing control, useful when you want to allow users to enter a sizeable amount of free-form text, for example a comment on a review or feedback form.
+  // The appendChild() method of the Node interface adds a node to the end of the list of children of a specified parent node.
 }
 function copyPassword() {
   const textarea = document.createElement("textarea");
